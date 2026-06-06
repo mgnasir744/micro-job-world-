6a1eaf715a63992cd7eeee4b77d8128fbebdaa69
mgnasir744 <mgnasir744@gmail.com>
2026-06-06 15:49:58 +0400

    Create AuthModal.js

---
+++ components/AuthModal.js
@@ -0,0 +1,11 @@
+import AuthModal from '../components/AuthModal';
+
+export default function Home() {
+  return (
+    <div>
+      {/* আপনার ল্যান্ডিং পেজের বাকি সব কোড এখানে থাকবে */}
+      
+      <AuthModal /> {/* পপআপটি এখানে রেন্ডার হবে */}
+    </div>
+  );
+}
